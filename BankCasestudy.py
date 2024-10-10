316b0f5f066fcac14dee3d477e0bd2a822f8c0ee
sriramganesh360 <113524626+sriramganesh360@users.noreply.github.com>
2024-10-11 01:38:37 +0530

    Add files via upload

---
+++ BankCasestudy.py
@@ -0,0 +1,172 @@
+1. ACCOUNT CREATION 
+2. PASBOOK 
+3. DEPOSIT 
+4. WITHDRAW 
+5. BALANCE ENQUIRY 
+6. FUND TRANSFER 
+7. TRANSCATION HISTORY
+8. EXIT
+Enter choice : 
+1
+Enter Account holder Name
+1
+Date of Birth 1 to 31
+1
+month of Birth
+1
+year of Birth
+1
+enter father name
+1
+enter Male or Female or others
+1
+You are major so we can proceed
+enter phone number
+1
+enter your userid 
+1
+ENTER YOUR 6 DIGIT PIN (ONLY DIGITS ARE ALLOWED)
+1
+Account Created Sucessfully 
+Account Nunmber  0
+1. ACCOUNT CREATION 
+2. PASBOOK 
+3. DEPOSIT 
+4. WITHDRAW 
+5. BALANCE ENQUIRY 
+6. FUND TRANSFER 
+7. TRANSCATION HISTORY
+8. EXIT
+Enter choice : 
+1
+Enter Account holder Name
+2
+Date of Birth 1 to 31
+2
+month of Birth
+2
+year of Birth
+2
+enter father name
+2
+enter Male or Female or others
+2
+You are major so we can proceed
+enter phone number
+2
+enter your userid 
+2
+ENTER YOUR 6 DIGIT PIN (ONLY DIGITS ARE ALLOWED)
+2
+Account Created Sucessfully 
+Account Nunmber  1
+1. ACCOUNT CREATION 
+2. PASBOOK 
+3. DEPOSIT 
+4. WITHDRAW 
+5. BALANCE ENQUIRY 
+6. FUND TRANSFER 
+7. TRANSCATION HISTORY
+8. EXIT
+Enter choice : 
+3
+enter account number: 
+0
+enter user_id : 
+1
+enter password: 
+1
+CURRENT BALANCE :  0
+enter deposit amount
+1000
+CURRENT BALANCE :  1000
+1. ACCOUNT CREATION 
+2. PASBOOK 
+3. DEPOSIT 
+4. WITHDRAW 
+5. BALANCE ENQUIRY 
+6. FUND TRANSFER 
+7. TRANSCATION HISTORY
+8. EXIT
+Enter choice : 
+3
+enter account number: 
+1
+enter user_id : 
+2
+enter password: 
+2
+CURRENT BALANCE :  0
+enter deposit amount
+2000
+CURRENT BALANCE :  2000
+1. ACCOUNT CREATION 
+2. PASBOOK 
+3. DEPOSIT 
+4. WITHDRAW 
+5. BALANCE ENQUIRY 
+6. FUND TRANSFER 
+7. TRANSCATION HISTORY
+8. EXIT
+Enter choice : 
+6
+enter account number
+1
+enter user_id : 
+2
+enter password: 
+2
+Enter your Account number
+1
+Enter reciever Account number
+0
+enter amount you want to send
+100
+100 Transferred Successfully from from 2
+to 1
+1. ACCOUNT CREATION 
+2. PASBOOK 
+3. DEPOSIT 
+4. WITHDRAW 
+5. BALANCE ENQUIRY 
+6. FUND TRANSFER 
+7. TRANSCATION HISTORY
+8. EXIT
+Enter choice : 
+7
+enter account number
+0
+enter user_id : 
+1
+enter password: 
+1
+10/11/2024 01:06:36: (100, 'Transferred from,', '2', ' and recieved by ', '1')
+1. ACCOUNT CREATION 
+2. PASBOOK 
+3. DEPOSIT 
+4. WITHDRAW 
+5. BALANCE ENQUIRY 
+6. FUND TRANSFER 
+7. TRANSCATION HISTORY
+8. EXIT
+Enter choice : 
+7
+enter account number
+1
+enter user_id : 
+2
+enter password: 
+2
+10/11/2024 01:06:36: (100, 'Transferred from,', '2', ' and recieved by ', '1')
+1. ACCOUNT CREATION 
+2. PASBOOK 
+3. DEPOSIT 
+4. WITHDRAW 
+5. BALANCE ENQUIRY 
+6. FUND TRANSFER 
+7. TRANSCATION HISTORY
+8. EXIT
+Enter choice : 
+
+Session Killed due to Timeout.
+Press Enter to exit terminal
